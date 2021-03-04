7df8abf44fc81818358e73c08a81a076001f32dc
Jeeva Kandasamy <jkandasa@gmail.com>
2021-03-04 07:42:52 +0530

    remove arm64/v8 platform
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,13 +18,13 @@ TARGET_BUILD=${TARGET_BUILD:-all-in-one}
 # build conatiner images
 if [[ "$TARGET_BINARY" == "core" ]]; then
   # build core image
-  docker buildx build --push --progress=plain --build-arg=GOPROXY=${GOPROXY} --platform linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/arm64,linux/amd64 --file docker/core.Dockerfile --tag ${IMAGE_CORE}:${IMAGE_TAG} .
+  docker buildx build --push --progress=plain --build-arg=GOPROXY=${GOPROXY} --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/amd64 --file docker/core.Dockerfile --tag ${IMAGE_CORE}:${IMAGE_TAG} .
 
 elif [[ "$TARGET_BINARY" == "gateway" ]]; then
   # build gateway image
-  docker buildx build --push --progress=plain --build-arg=GOPROXY=${GOPROXY} --platform linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/arm64,linux/amd64 --file docker/gateway.Dockerfile --tag ${IMAGE_ALL_IN_ONE}:${IMAGE_TAG} .
+  docker buildx build --push --progress=plain --build-arg=GOPROXY=${GOPROXY} --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/amd64 --file docker/gateway.Dockerfile --tag ${IMAGE_ALL_IN_ONE}:${IMAGE_TAG} .
 
 else
   # build all-in-one image
-  docker buildx build --push --progress=plain --build-arg=GOPROXY=${GOPROXY} --platform linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/arm64,linux/amd64 --file docker/all-in-one.Dockerfile --tag ${IMAGE_GATEWAY}:${IMAGE_TAG} .
+  docker buildx build --push --progress=plain --build-arg=GOPROXY=${GOPROXY} --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/amd64 --file docker/all-in-one.Dockerfile --tag ${IMAGE_GATEWAY}:${IMAGE_TAG} .
 fi
