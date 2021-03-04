e8f31394e53706d65bfc24526d76ad922c67528d
Jeeva Kandasamy <jkandasa@gmail.com>
2021-03-04 09:18:50 +0530

    update cache style
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# get backend branch details
-BACKEND_BRANCH=`git rev-parse --abbrev-ref HEAD`
-
-# build web console
-git submodule update --init --recursive
-git submodule update --remote
 cd console-web
-git checkout $BACKEND_BRANCH  # sync with backend branch for webconsole
 yarn install
 CI=false yarn build
 cd ../
