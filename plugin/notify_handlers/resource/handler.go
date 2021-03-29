a3f54af007ea23273307d63151861019347e3a26
Jeeva Kandasamy <jkandasa@gmail.com>
2021-03-29 10:41:58 +0530

    support resource handler preDelay
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- plugin/notify_handlers/resource/handler.go
+++ plugin/notify_handlers/resource/handler.go
@@ -1,12 +1,15 @@
 package resource
 
 import (
+	"fmt"
 	"strings"
+	"time"
 
 	"github.com/mycontroller-org/backend/v2/pkg/json"
 	"github.com/mycontroller-org/backend/v2/pkg/model"
 	handlerML "github.com/mycontroller-org/backend/v2/pkg/model/notify_handler"
 	rsML "github.com/mycontroller-org/backend/v2/pkg/model/resource_service"
+	coreScheduler "github.com/mycontroller-org/backend/v2/pkg/service/core_scheduler"
 	busUtils "github.com/mycontroller-org/backend/v2/pkg/utils/bus_utils"
 	variableUtils "github.com/mycontroller-org/backend/v2/pkg/utils/variables"
 	"go.uber.org/zap"
@@ -17,11 +20,18 @@ type Client struct {
 	HandlerCfg *handlerML.Config
 }
 
+const (
+	schedulePrefix = "resource_handler"
+)
+
 // Start handler implementation
 func (c *Client) Start() error { return nil }
 
 // Close handler implementation
-func (c *Client) Close() error { return nil }
+func (c *Client) Close() error {
+	c.unloadAll()
+	return nil
+}
 
 // State implementation
 func (c *Client) State() *model.State {
@@ -59,8 +69,57 @@ func (c *Client) Post(data map[string]interface{}) error {
 			continue
 		}
 
+		if rsData.PreDelay != "" {
+			delayDuration, err := time.ParseDuration(rsData.PreDelay)
+			if err != nil {
+				return fmt.Errorf("invalid preDelay. name:%s, preDelay:%s", name, rsData.PreDelay)
+			}
+			if delayDuration > 0 {
+				c.schedule(name, rsData)
+				continue
+			}
+		}
+
 		zap.L().Debug("about to perform an action", zap.String("rawData", stringValue), zap.Any("finalData", rsData))
 		busUtils.PostToResourceService("resource_selector", rsData, rsML.TypeResourceActionBySelector, rsML.CommandSet)
 	}
 	return nil
+}
+
+// preDelay scheduler helper
+
+func (c *Client) getScheduleTriggerFunc(name string, rsData handlerML.ResourceData) func() {
+	return func() {
+		// disable the schedule
+		c.unschedule(name)
+
+		// call the resource action
+		zap.L().Debug("scheduler triggere. about to perform an action", zap.String("name", name), zap.Any("rsData", rsData))
+		busUtils.PostToResourceService("resource_selector", rsData, rsML.TypeResourceActionBySelector, rsML.CommandSet)
+	}
+}
+
+func (c *Client) schedule(name string, rsData handlerML.ResourceData) {
+	c.unschedule(name) // removes the existing schedule, if any
+	schedulerID := c.getScheduleID(name)
+	cronSpec := fmt.Sprintf("@every %s", rsData.PreDelay)
+	err := coreScheduler.SVC.AddFunc(schedulerID, cronSpec, c.getScheduleTriggerFunc(name, rsData))
+	if err != nil {
+		zap.L().Error("error on adding schedule", zap.Error(err))
+	}
+	zap.L().Debug("added a schedule", zap.String("name", name), zap.String("schedulerID", schedulerID), zap.Any("resourceData", rsData))
+}
+
+func (c *Client) unschedule(name string) {
+	schedulerID := c.getScheduleID(name)
+	coreScheduler.SVC.RemoveFunc(schedulerID)
+	zap.L().Debug("removed a schedule", zap.String("name", name), zap.String("schedulerID", schedulerID))
+}
+
+func (c *Client) unloadAll() {
+	coreScheduler.SVC.RemoveWithPrefix(fmt.Sprintf("%s_%s", schedulePrefix, c.HandlerCfg.ID))
+}
+
+func (c *Client) getScheduleID(name string) string {
+	return fmt.Sprintf("%s_%s_%s", schedulePrefix, c.HandlerCfg.ID, name)
 }
