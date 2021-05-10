a22391ec796581aa30cf6c3f14fd8819171c269f
Jeeva Kandasamy <jkandasa@gmail.com>
2021-05-11 00:11:33 +0530

    changes in githup action workflow
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

---
+++ scripts/generate_binaries.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%S%:z'`
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+GIT_SHA=`git rev-parse HEAD`
+GIT_SHA_SHORT=`git rev-parse --short HEAD`
+VERSION_PKG="github.com/mycontroller-org/backend/v2/pkg/version"
+
+LD_FLAGS="-X $VERSION_PKG.version=$GIT_BRANCH -X $VERSION_PKG.buildDate=$BUILD_DATE -X $VERSION_PKG.gitCommit=$GIT_SHA"
+
+# different architectures
+ANDROID_ARCHITECTURES="arm"
+DARWIN_ARCHITECTURES="arm arm64 386 amd64"
+LINUX_ARCHITECTURES="arm arm64 386 amd64"
+WINDOWS_ARCHITECTURES="386 amd64"
+
+build_binaries() {
+  os=$1
+  arch=$2
+  mkdir -p binaries
+  GOOS=${os} GOARCH=${arch} go build -o -v binaries/mycontroller-all-in-one_${os}_${arch} -ldflags "$LD_FLAGS" cmd/all-in-one/main.go
+  GOOS=${os} GOARCH=${arch} go build -o -v binaries/mycontroller-core_${os}_${arch} -ldflags "$LD_FLAGS" cmd/core/main.go
+  GOOS=${os} GOARCH=${arch} go build -o -v binaries/mycontroller-gateway_${os}_${arch} -ldflags "$LD_FLAGS" cmd/gateway/main.go
+}
+
+# compile for android
+for arch in ${ANDROID_ARCHITECTURES}
+do
+  build_binaries "android" ${arch}
+done
+
+# compile for darwin
+for arch in ${DARWIN_ARCHITECTURES}
+do
+  build_binaries "darwin" ${arch}
+done
+
+# compile for linux
+for arch in ${LINUX_ARCHITECTURES}
+do
+  build_binaries "linux" ${arch}
+done
+
+
+# compile for windows
+for arch in ${WINDOWS_ARCHITECTURES}
+do
+  build_binaries "windows" ${arch}
+done
