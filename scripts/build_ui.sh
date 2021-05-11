0eea381a5819cab07a7d194122119c8a10b92f09
Jeeva Kandasamy <jkandasa@gmail.com>
2021-05-11 07:00:11 +0530

    local build scripts
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+BUILDS_DIR=builds
+
+# sync the web console repository
+./scripts/setup_ui.sh
+
+# build web console
 cd console-web
 yarn install
 CI=false yarn build
 cd ../
+
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+mkdir -p ${BUILDS_DIR}
+cp console-web/build ${BUILDS_DIR}/web_console -r
+cd ${BUILDS_DIR}
+zip -r -q web_console_${GIT_BRANCH}.zip  web_console
+tar czf web_console_${GIT_BRANCH}.tar.gz web_console
+
+cd ..
+# remove copied ui build files
+rm ${BUILDS_DIR}/web_console -rf
--- scripts/generate_builds.sh
+++ scripts/generate_builds.sh
@@ -3,6 +3,10 @@
 # this script used to generate binary files
 # should be executed from the root locations of the repository
 
+BUILD_DIR=builds
+# clean builds directory
+rm ${BUILD_DIR}/* -rf
+
 BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%S%:z'`
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 GIT_SHA=`git rev-parse HEAD`
@@ -11,21 +15,21 @@ VERSION_PKG="github.com/mycontroller-org/backend/v2/pkg/version"
 
 LD_FLAGS="-X $VERSION_PKG.version=$GIT_BRANCH -X $VERSION_PKG.buildDate=$BUILD_DATE -X $VERSION_PKG.gitCommit=$GIT_SHA"
 
-# different architectures
-LINUX_ARCHITECTURES="arm arm64 386 amd64"
-WINDOWS_ARCHITECTURES="386 amd64"
-
 build_binaries() {
   os=$1
   arch=$2
-  mkdir -p binaries
-  GOOS=${os} GOARCH=${arch} go build -o binaries/mycontroller-all-in-one_${os}_${arch} -ldflags "$LD_FLAGS" cmd/all-in-one/main.go
-  GOOS=${os} GOARCH=${arch} go build -o binaries/mycontroller-core_${os}_${arch} -ldflags "$LD_FLAGS" cmd/core/main.go
-  GOOS=${os} GOARCH=${arch} go build -o binaries/mycontroller-gateway_${os}_${arch} -ldflags "$LD_FLAGS" cmd/gateway/main.go
+  mkdir -p ${BUILD_DIR}
+  GOOS=${os} GOARCH=${arch} go build -o ${BUILD_DIR}/mycontroller-all-in-one_${GIT_BRANCH}_${os}-${arch} -ldflags "$LD_FLAGS" cmd/all-in-one/main.go
+  GOOS=${os} GOARCH=${arch} go build -o ${BUILD_DIR}/mycontroller-core_${GIT_BRANCH}_${os}-${arch} -ldflags "$LD_FLAGS" cmd/core/main.go
+  GOOS=${os} GOARCH=${arch} go build -o ${BUILD_DIR}/mycontroller-gateway_${GIT_BRANCH}_${os}_${arch} -ldflags "$LD_FLAGS" cmd/gateway/main.go
 }
 
 # download dependencies
 go mod tidy
+
+# different architectures
+LINUX_ARCHITECTURES="arm arm64 386 amd64"
+WINDOWS_ARCHITECTURES="386 amd64"
  
 # compile for linux
 for arch in ${LINUX_ARCHITECTURES}
@@ -33,9 +37,13 @@ do
   build_binaries "linux" ${arch}
 done
 
-
 # compile for windows
 for arch in ${WINDOWS_ARCHITECTURES}
 do
   build_binaries "windows" ${arch}
 done
+
+# generate UI builds
+if [ "$BUILD_UI" = true ] ; then
+  ./scripts/build_ui.sh
+fi
