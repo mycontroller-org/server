de42c33c3271f34b27a625d29f6bfd929e4dbc42
Jeeva Kandasamy <jkandasa@gmail.com>
2021-06-06 19:39:53 +0530

    web console branch update
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/setup_web_console.sh
+++ scripts/setup_web_console.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # get backend branch details
-BACKEND_BRANCH=`git rev-parse --abbrev-ref HEAD`
+BACKEND_BRANCH=`git symbolic-ref -q --short HEAD || git describe --tags --exact-match`
+
+echo "Backend branch/tag: ${BACKEND_BRANCH}"
 
 # build web console
 git submodule update --init --recursive
 git submodule update --remote
 cd console-web
-git checkout $BACKEND_BRANCH  # sync with backend branch for webconsole
+git fetch --all --tags
+git checkout $BACKEND_BRANCH  # sync web console with the backend version
 cd ../
